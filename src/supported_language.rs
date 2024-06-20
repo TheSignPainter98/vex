717581f574688601aa818d6876ca20a3bb6d4c5a
kcza <ed@kcza.net>
2024-06-21 00:40:03 +0200

    Test ignore ranges

--- src/supported_language.rs
+++ src/supported_language.rs
@@ -4,10 +4,11 @@ use allocative::Allocative;
 use clap::Subcommand;
 use dupe::Dupe;
 use enum_map::{Enum, EnumMap};
+use indoc::indoc;
 use lazy_static::lazy_static;
 use serde::{Deserialize as Deserialise, Serialize as Serialise};
 use strum::{EnumIter, IntoEnumIterator};
-use tree_sitter::Language;
+use tree_sitter::{Language, Query};
 
 use crate::{error::Error, result::Result};
 
@@ -63,6 +64,35 @@ impl SupportedLanguage {
             Self::Rust => tree_sitter_rust::language(),
         })
     }
+
+    pub fn ignore_query(&self) -> &Query {
+        lazy_static! {
+            static ref IGNORE_QUERIES: EnumMap<SupportedLanguage, OnceLock<Query>> =
+                SupportedLanguage::iter()
+                    .zip(iter::repeat_with(OnceLock::new))
+                    .collect();
+        }
+
+        IGNORE_QUERIES[*self].get_or_init(|| {
+            let raw = match self {
+                Self::C | Self::Cpp | Self::Go | Self::Python => indoc! {r#"
+                    (
+                        (comment) @ignore_tag (#match-any? @ignore_tag "^// vex:ignore" "^/\* vex:ignore")
+                        .
+                        (_) @ignore
+                    )
+                "#},
+                Self::Rust => indoc! {r#"
+                    (
+                        (line_comment) @ignore_tag (#match? @ignore_tag "^// vex:ignore")
+                        .
+                        (_) @ignore
+                    )
+                "#},
+            };
+            Query::new(self.ts_language(), raw).expect("internal error: ignore query invalid")
+        })
+    }
 }
 
 impl FromStr for SupportedLanguage {
@@ -88,7 +118,12 @@ impl Display for SupportedLanguage {
 
 #[cfg(test)]
 mod test {
+    use std::ops::Range;
+
     use strum::IntoEnumIterator;
+    use tree_sitter::QueryCursor;
+
+    use crate::{source_file::ParsedSourceFile, source_path::SourcePath};
 
     use super::*;
 
@@ -98,5 +133,130 @@ mod test {
             assert_eq!(lang, lang.name().parse()?);
         }
         Ok(())
+    }
+
+    #[test]
+    fn ignore_extraction() {
+        Test::language(SupportedLanguage::C)
+            .with_source(indoc! {r#"
+                void main() {
+                    // vex:ignore
+                    int x[] = {
+                        1,
+                        2,
+                        3,
+                    };
+                    /* vex:ignore */
+                    int y[] = {
+                        1,
+                        2,
+                        3,
+                    };
+                }
+            "#})
+            .ignores(&[18..31, 36..87, 92..108, 113..164]);
+        Test::language(SupportedLanguage::Cpp)
+            .with_source(indoc! {r#"
+                void main() {
+                    // vex:ignore
+                    vector<int> x {
+                        1,
+                        2,
+                        3,
+                    };
+                    /* vex:ignore */
+                    vector<int> y = {
+                        1,
+                        2,
+                        3,
+                    };
+                }
+            "#})
+            .ignores(&[18..31, 36..91, 96..112, 117..174]);
+        Test::language(SupportedLanguage::Go)
+            .with_source(indoc! {r#"
+                package main
+
+                func main() {
+                    // vex:ignore
+                    x := []int{
+                        1,
+                        2,
+                        3,
+                    }
+                }
+            "#})
+            .ignores(&[32..45, 50..100]);
+        Test::language(SupportedLanguage::Python)
+            .with_source(indoc! {r#"
+                def main():
+                    # vex:ignore
+                    x = [
+                        1,
+                        2,
+                        3,
+                    ]
+            "#})
+            .ignores(&[16..28, 33..77]);
+        Test::language(SupportedLanguage::Rust)
+            .with_source(indoc! {r#"
+                fn main() {
+                    // vex:ignore
+                    let x = [
+                        1,
+                        2,
+                        3,
+                    ];
+                }
+            "#})
+            .ignores(&[16..29, 34..83]);
+
+        // Test structs
+        #[must_use]
+        struct Test {
+            language: SupportedLanguage,
+            source: Option<&'static str>,
+        }
+
+        impl Test {
+            fn language(language: SupportedLanguage) -> Self {
+                Self {
+                    language,
+                    source: None,
+                }
+            }
+
+            fn with_source(mut self, source: &'static str) -> Self {
+                self.source = Some(source);
+                self
+            }
+
+            fn ignores(self, ranges: &[Range<usize>]) {
+                self.setup();
+
+                let source_file = ParsedSourceFile::new_with_content(
+                    SourcePath::new_in("test.file".into(), "".into()),
+                    self.source.unwrap(),
+                    self.language,
+                )
+                .unwrap();
+
+                let query = self.language.ignore_query();
+                let ignore_ranges: Vec<_> = QueryCursor::new()
+                    .matches(
+                        query,
+                        source_file.tree.root_node(),
+                        source_file.content.as_bytes(),
+                    )
+                    .flat_map(|qmatch| qmatch.captures)
+                    .map(|qcap| qcap.node.byte_range())
+                    .collect();
+                assert_eq!(ranges, ignore_ranges);
+            }
+
+            fn setup(&self) {
+                eprintln!("running {} test...", self.language);
+            }
+        }
     }
 }
